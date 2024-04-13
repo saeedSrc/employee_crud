1be5e8f9122b8f6b2e9c79057a50b4ed87ecb876
saeed rasooli <saeed_rasooli@yahoo.com>
2024-04-13 19:16:13 +0330

    getting employees list

---
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        List<Employee> theEmployee = employeeService.findAll();
+
+        model.addAttribute("employees", theEmployee);
+
+        return "list-employees";
+    }
+}
